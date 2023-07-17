0574f301b2ba911d76c3216bbd318dace5d3acd5
nursyazana22 <139740550+nursyazana22@users.noreply.github.com>
2023-07-17 16:43:33 +0800

    PartBQuestion(b).c

--- PartBQuestion(b).c
+++ PartBQuestion(b).c
@@ -1,15 +1,18 @@
 import socket
+import random
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
 
-server_address = ('localhost', 8596)
+  client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client_socket.connect(("localhost", 8080))
 
-client_socket.connect(server_address)
+  random_number = random.randint(1, 100)
 
-number_bytes = client_socket.recv(1024)
+  client_socket.sendall(str(random_number).encode("utf-8")
+  
+  received_random_number = client_socket.recv(1024).decode("utf-8")
 
-random_number = int(number_bytes.decode('utf-8'))
+  print("The random number is:", received_random_number)
 
-print('Received random number:', random_number)
-
-client_socket.close()
+if __name__ == "__main__":
+  main()
