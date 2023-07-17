09410f4020625ec7fb1714393bf770909da5d712
nursyazana22 <139740550+nursyazana22@users.noreply.github.com>
2023-07-17 16:21:34 +0800

    PartBQuestion(a).c

--- PartBQuestion(a).c
+++ PartBQuestion(a).c
@@ -1,11 +1,9 @@
 import random
 
 def get_random_number():
-  """Returns a random number between 50,000 and 80,000."""
   return random.randint(50000, 80000)
 
 def main():
-  """Starts the server and passes one random number."""
   number = get_random_number()
   print("Passing random number:", number)
 
